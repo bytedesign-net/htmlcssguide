6d760aaeaa5bd3a34824e9ed01a7951a38726af4
notec <31621646+curledtail@users.noreply.github.com>
2023-01-19 00:37:15 +0900

    ローカルナビ

--- js/script.js
+++ js/script.js
@@ -38,15 +38,19 @@ jQuery(document).ready(function () {
     });
 });
 
-$(function () {
-  /*=== サイドバーの見出し生成  ===*/
-  $("h2").each(function (i) {
+jQuery(function () {
+  /*=== ローカルナビ生成 ===*/
+  jQuery("h2").each(function (i) {
     var heading = $(this);
     var headingId = "headline_" + (i + 1);
     var headingText = heading.text();
     heading.attr("id", headingId);
-    $(".nav-heading").append(
-      "<li class=''><a href='#" + headingId + "'>" + headingText + "</a></li>"
+    jQuery(".panel").append(
+      '<a class="panel-block" href=\'#' +
+        headingId +
+        "'>" +
+        headingText +
+        "</a>"
     );
   });
 });
